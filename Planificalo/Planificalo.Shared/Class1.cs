7f38fa15158e080bbd7e32e2b278bb5c8f843037
Juan Camilo Arboleda <juan.arboleda@gmail.com>
2024-09-09 12:00:30 -0500

    New Entities and migration

---
+++ Planificalo/Planificalo.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Planificalo.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Planificalo/Planificalo.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Planificalo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
