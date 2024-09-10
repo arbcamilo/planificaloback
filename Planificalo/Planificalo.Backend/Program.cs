ad68deec9937596f06868f7cf76cafcc1bda904f
Juan Camilo Arboleda <juan.arboleda@gmail.com>
2024-09-09 21:09:22 -0500

    change database name string

--- Planificalo/Planificalo.Backend/Program.cs
+++ Planificalo/Planificalo.Backend/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
