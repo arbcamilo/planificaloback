e64d482d3eef4ac5e1a42b9a39d19e222cd5d03c
paolamolina25 <paolamolina1143365@correo.itm.edu.co>
2024-09-22 11:08:48 -0500

    Feature/paginacion (#2)
    * agrego la controladora de servicios
    
    * agrego las controladoras de eventos y cotizaciones

---
+++ Planificalo/Planificalo.Backend/Controllers/EventosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planificalo.Backend.UnitsOfWork.Interfaces;
+using Planificalo.Shared.Entities;
+
+namespace Planificalo.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/admin/[controller]")]
+    public class EventosController : GenericController<Evento>
+    {
+        public EventosController(IGenericUnitOfWork<Evento> unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
