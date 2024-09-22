91599dcd7ce3ede102994df36db0fd5d029ce65c
PAOLA ANDREA MOLINA RUEDA <paolamolina1143365@correo.itm.edu.co>
2024-09-22 10:41:40 -0500

    agrego la controladora de servicios

---
+++ Planificalo/Planificalo.Backend/Controllers/ServiciosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planificalo.Backend.UnitsOfWork.Interfaces;
+using Planificalo.Shared.Entities;
+
+namespace Planificalo.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/admin/[controller]")]
+    public class ServiciosController : GenericController<Servicio>
+    {
+        public ServiciosController(IGenericUnitOfWork<Servicio> unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
