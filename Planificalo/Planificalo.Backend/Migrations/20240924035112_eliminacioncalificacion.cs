14f8db6e82912aba2c0791ebed8e353c18bce29a
paolamolina25 <paolamolina1143365@correo.itm.edu.co>
2024-09-23 22:53:32 -0500

    Feature/pam paginacion (#8)
    * modifico campo en el shared servicio
    
    * agrego las controladores faltantes
    
    * subo scrípt 2
    
    * actualizacion tablas proveedores

---
+++ Planificalo/Planificalo.Backend/Migrations/20240924035112_eliminacioncalificacion.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planificalo.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class eliminacioncalificacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calificacion",
+                table: "Proveedores");
+
+            migrationBuilder.RenameColumn(
+                name: "cantidad",
+                table: "Servicios",
+                newName: "Cantidad");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cantidad",
+                table: "Servicios",
+                newName: "cantidad");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Calificacion",
+                table: "Proveedores",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
