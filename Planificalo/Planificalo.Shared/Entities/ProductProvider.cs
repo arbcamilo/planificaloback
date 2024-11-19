4f78cdf2b34620922ffc79ef22e653246a4d469f
PAOLA ANDREA MOLINA RUEDA <paolamolina1143365@correo.itm.edu.co>
2024-11-18 21:44:53 -0500

    changes relation services and products

--- Planificalo/Planificalo.Shared/Entities/ProductProvider.cs
+++ Planificalo/Planificalo.Shared/Entities/ProductProvider.cs
@@ -3,9 +3,7 @@ namespace Planificalo.Shared.Entities
     public class ProductProvider
     {
         public int ProviderId { get; set; }
-        public Provider Provider { get; set; }
 
         public int ProductId { get; set; }
-        public Product Product { get; set; }
     }
 }
