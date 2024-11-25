854cd82ee9ef26b348301f7c541e9f2db606d676
Juan Camilo Arboleda <juan.arboleda@gmail.com>
2024-11-24 21:37:32 -0500

    change query by number

--- Planificalo/Planificalo.Shared/Entities/Event.cs
+++ Planificalo/Planificalo.Shared/Entities/Event.cs
@@ -9,7 +9,7 @@ namespace Planificalo.Shared.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public DateOnly Date { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public string Location { get; set; }
         public int EventTypeId { get; set; }
         public string IsPrivate { get; set; }
