f752b0a4a54dd51a3ad30775be7914629cdaad4e
PAOLA ANDREA MOLINA RUEDA <paolamolina1143365@correo.itm.edu.co>
2024-11-25 22:37:48 -0500

    fix servicies entities

--- Planificalo/Planificalo.Backend/Controllers/ServiceProvidersController.cs
+++ Planificalo/Planificalo.Backend/Controllers/ServiceProvidersController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Planificalo.Backend.Data;
 using Planificalo.Backend.UnitsOfWork.Interfaces;
 using Planificalo.Shared.Entities;
+using Planificalo.Shared.Responses;
 using System.Threading.Tasks;
 using ServiceProvider = Planificalo.Shared.Entities.ServiceProvider;
 
@@ -15,8 +16,34 @@ namespace Planificalo.Backend.Controllers
     [Route("api/admin/[controller]")]
     public class ServiceProvidersController : GenericController<ServiceProvider>
     {
-        public ServiceProvidersController(IGenericUnitOfWork<ServiceProvider> unitOfWork) : base(unitOfWork)
+        private readonly DataContext _context;
+
+        public ServiceProvidersController(IGenericUnitOfWork<ServiceProvider> unitOfWork, DataContext context) : base(unitOfWork)
         {
+            _context = context;
+        }
+
+        [HttpGet("ByDocumentNumber/{documentNumber}")]
+        public async Task<ActionResult<ActionResponse<IEnumerable<ServiceProvider>>>> GetByDocumentNumber(int documentNumber)
+        {
+            var providers = await _context.ServiceProviders
+          .Where(pp => pp.ProviderId == documentNumber)
+          .ToListAsync();
+
+            if (providers == null || !providers.Any())
+            {
+                return NotFound(new ActionResponse<IEnumerable<ServiceProvider>>
+                {
+                    Success = false,
+                    Message = "No providers found with the given document number."
+                });
+            }
+
+            return Ok(new ActionResponse<IEnumerable<ServiceProvider>>
+            {
+                Success = true,
+                Entity = providers
+            });
         }
     }
 }
