76ec4e8f5552c7c95184fdfb68a8806b1468a594
PAOLA ANDREA MOLINA RUEDA <paolamolina1143365@correo.itm.edu.co>
2024-11-25 23:07:19 -0500

    fix datacontext services

---
+++ Planificalo/Planificalo.Backend/Migrations/20241126040353_fixdatacontextserv.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planificalo.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixdatacontextserv : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ServiceProviders",
+                table: "ServiceProviders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductProviders",
+                table: "ProductProviders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ServiceProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ProductProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ServiceProviders",
+                table: "ServiceProviders",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductProviders",
+                table: "ProductProviders",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ServiceProviders",
+                table: "ServiceProviders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductProviders",
+                table: "ProductProviders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ServiceProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ProductProviders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ServiceProviders",
+                table: "ServiceProviders",
+                columns: new[] { "ProviderId", "ServiceId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductProviders",
+                table: "ProductProviders",
+                columns: new[] { "ProviderId", "ProductId" });
+        }
+    }
+}
